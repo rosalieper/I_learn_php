36f894554995eb3e7a19da6c0ca70639680a9911
unknown <calebmoses1111@gmail.com>
2018-10-31 23:33:50 +0100

    i added a blog.php and a home.php file

---
+++ home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<div class="background-image"><style>
+			body 
+			{
+				background-image: url('im.JPG');
+				background-size: cover;
+				
+			}
+
+		</style>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<title>Super_Blog</title>
+
+</head>
+<body>
+
+<center><h1><b>Welcom to my super blog</b></h1></center>
+<nav>   
+			 <ul>
+		     	<li><a href="home.php">Home</a></li>
+		     	<li><a href="sign-up.php">sign_up</a></li>
+		    	 <li><a href="log-in.php">log_in</a></li>    
+			 </ul>
+		</nav></br></br>
+<h1><b><i>HOME</i></b></h1>
+	
+
+
+
+
+
+</body>
+</html>
